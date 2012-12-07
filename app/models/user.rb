02a40e2f29dfadd13cde1301ee6de438cf816f49
Ting Keng Boon <chtng@singnet.com.sg>
2012-12-07 21:46:46 +0800

    Added basic validation for NRIC

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   validates :email, :name, :nric, :password, :presence => true
   validates :nric, :email, :uniqueness => true
   validates :email, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+  validates :nric, :format => { :with => /^S\d{7}[a-zA-z]$/i }
 end
